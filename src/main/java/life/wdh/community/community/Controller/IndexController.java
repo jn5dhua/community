df07decceaa734a947d0660291af6ec6326af639
jn5dhua <wdhua001@163.com>
2019-12-04 09:02:58 +0800

    add cookies

--- src/main/java/life/wdh/community/community/Controller/IndexController.java
+++ src/main/java/life/wdh/community/community/Controller/IndexController.java
@@ -1,13 +1,35 @@
 package life.wdh.community.community.Controller;
 
+import life.wdh.community.community.mapper.UserMapper;
+import life.wdh.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String Hello(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
